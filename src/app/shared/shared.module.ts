6c21520c996c876e2acb8083b5183ca718679e26
kabir-webDev <kabir.webdev@gmail.com>
2022-03-14 16:13:08 +0600

    Sidenav and Header on progress

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ErrorComponent } from './components/error/error.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [ErrorComponent],
+  declarations: [],
   imports: [CommonModule, RouterModule],
   exports: [],
 })
